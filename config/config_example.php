30854e3381ae11c7f6be88e1b69af5e855647814
carsonlius <carsonlius@163.com>
2018-03-27 20:23:59 +0800

    modify:配置项

---
+++ config/config_example.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'mysql' => [
+        'gray_db' => [
+            'host' => 'x',
+            'port' => 'x',
+            'user' => 'x',
+            'password' => 'x',
+            'dbname' => 'x',
+            'charset' => 'x'
+        ]
+    ],
+];
